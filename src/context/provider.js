4458ff6ce125bb1abf66796ea8899003a0fde567
Sayan Samanta <samantasayan23@gmail.com>
2022-02-11 17:42:54 +0530

    initial

---
+++ src/context/provider.js
@@ -0,0 +1,33 @@
+import React, {useState, createContext} from "react";
+
+export const GlobalContext = createContext({});
+
+export const GlobalProvider = ({children}) => {
+
+    const [userData, setUserData] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [softLoading, setSoftLoading] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [designation, setDesignation] = useState("");
+    const [toastAppear, setToastAppear] = useState(false);
+    const [transferMod, setTransferMod] = useState(false);
+    const [modalId, setModalId] = useState({});
+    // web3 stuffs
+    const [acct, setAcct] = useState(null);
+
+    return (
+        <GlobalContext.Provider value={{
+            solid: [loading, setLoading], 
+            soft: [softLoading, setSoftLoading],
+            modal: [modalOpen, setModalOpen],
+            desig: [designation, setDesignation],
+            toast: [toastAppear, setToastAppear],
+            transfer: [transferMod, setTransferMod],
+            modalID: [modalId, setModalId],
+            web3Ac: [acct, setAcct],
+            UserData: [userData, setUserData]
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
