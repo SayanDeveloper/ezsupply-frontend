2508c285629b73c4e15235e87b58f07f503368ea
Sayan Samanta <samantasayan23@gmail.com>
2022-10-07 02:49:19 +0530

    updated network to goerli

--- truffle-config.js
+++ truffle-config.js
@@ -13,6 +13,10 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/"+process.env.INFURA_API_KEY),
       network_id: 4,     
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/"+process.env.GOERLI_API_KEY),
+      network_id: 4,     
     }
   }
 
